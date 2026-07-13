952edbb2c2c8797981b6ca4a670bee8afd999370
MINHYEOK SEO <cbssmh@gmail.com>
2026-07-13 15:04:30 +0900

    feat: establish v2 persistence layer

--- src/test/java/com/minibank/mini_core_banking/MiniCoreBankingApplicationTests.java
+++ src/test/java/com/minibank/mini_core_banking/MiniCoreBankingApplicationTests.java
@@ -2,9 +2,26 @@ package com.minibank.mini_core_banking;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class MiniCoreBankingApplicationTests {
+
+	@Container
+	static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17");
+
+	@DynamicPropertySource
+	static void configureDataSource(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+		registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+	}
 
 	@Test
 	void contextLoads() {
